bfe38e9acd6ee4f2a7e85b743e3a7b63cfe45f87
Ian Horsman <ianphorsman@gmail.com>
2016-10-28 20:53:09 -0400

    Started work on Video component

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -15,8 +15,12 @@ class App extends React.Component {
                 </MainMenu>
             </header>
             <main>
-                <Video>
-                </Video>
+                <section className="row" id="Affectiva">
+                    <div classname="col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+                        <VideoContainer>
+                        </VideoContainer>
+                    </div>
+                </section>
             </main>
             <footer>
             </footer>
--- app/assets/javascripts/components/video.es6.jsx
+++ app/assets/javascripts/components/video.es6.jsx
@@ -1,6 +1,48 @@
 class Video extends React.Component {
+
+    startCamera() {
+        return(
+            <div id="affectiva-video-container">
+                <p>Camera Enabled</p>
+            </div>
+        )
+    }
+
+    requestCamera() {
+        return(
+            <button onClick={this.props.enableCamera}>Enable Camera Feed</button>
+        )
+    }
+    
+    renderStream() {
+        if (this.props.cameraEnabled) {
+            return this.startCamera();
+        } else {
+            return this.requestCamera();
+        }
+    }
+
+    loadAffectivaDetector() {
+        let videoRoot = $('affectiva-video-container')[0];
+        let width = 640;
+        let height = 480;
+        let faceMode = affdex.FaceDetectorMode.LARGE_FACES;
+        let detector = new affdex.CameraDetector(videoRoot, width, height, faceMode);
+
+        detector.detectAllEmotions();
+        detector.detectAllExpressions();
+        detector.detectAllAppearance();
+    }
+
+    componentDidMount() {
+        if (this.props.cameraEnabled) {
+            this.loadAffectivaDetector();
+        }
+    }
+    
+    
   render () {
-    return <div />;
+    return this.renderStream();
   }
 }
 
